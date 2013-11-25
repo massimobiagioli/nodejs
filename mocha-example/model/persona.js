3d6b52e33d32d538bb2bb7f0dcdaedb7c96fe0ff
massimo <github@massimobiagioli.it>
2013-11-25 14:15:06 +0100

    Aggiunti test

--- mocha-example/model/persona.js
+++ mocha-example/model/persona.js
@@ -3,6 +3,8 @@ var	Persona = function(data) {
 	this.cognome = data.cognome;
 	this.dataNascita = data.dataNascita;
 	this.luogoNascita = data.luogoNascita;
+	this.codiceFiscale = data.codiceFiscale;
+	this.extraInfo = data.extraInfo;
 };
 
 module.exports = {
