d020033a0297cd70eb6c27f14bf38ddf35e131b4
massimo <github@massimobiagioli.it>
2014-02-26 17:42:49 +0100

    cors

--- rpi-unit/server.js
+++ rpi-unit/server.js
@@ -7,6 +7,7 @@
 var express = require('express'),
 	serverConfig = require('./config/server'),
 	io = require('socket.io'),
+	cors = require('cors'),
 	crudRoutes = require('./routes/crud');
 
 /* Crea applicazione Express */
@@ -19,14 +20,16 @@ app.use(express.logger());
 app.use(express.json());
 app.use(express.urlencoded());
 app.use(express.methodOverride());
+app.use(cors());
+app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
 /* Configura Routes */
 app.get('/api/list/:tableKey', crudRoutes.list); 
-app.get('/api/get/:tableKey/:tableId', crudRoutes.get); 
+app.get('/api/get/:tableKey/:tableId(\\d+)', crudRoutes.get); 
 app.post('/api/insert/:tableKey', crudRoutes.insert); 
-app.put('/api/update/:tableKey/:tableId', crudRoutes.update); 
-app.del('/api/delete/:tableKey/:tableId', crudRoutes.del);  
+app.put('/api/update/:tableKey/:tableId(\\d+)', crudRoutes.update); 
+app.del('/api/delete/:tableKey/:tableId(\\d+)', crudRoutes.del);  
 
 /* Crea server */
 io.listen(app.listen(app.get('port')));
