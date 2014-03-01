b21312a9f77e663b6f9916083e46a5d34a85254d
massimobiagioli <github@massimobiagioli.it>
2014-03-01 10:14:39 +0100

    rifiniture

--- arduino-ledRGB/server.js
+++ arduino-ledRGB/server.js
@@ -1,5 +1,12 @@
 var express = require('express'),
-	http = require('http');
+	http = require('http'),
+	serialport = require("serialport");
+
+var SerialPort = serialport.SerialPort; 
+var sp = new SerialPort("/dev/ttyACM0", {
+  baudrate: 9600,
+  parser: serialport.parsers.readline("\n")
+});
 
 var app = express();
 app.set('port', process.env.PORT || 3000);
@@ -10,12 +17,27 @@ app.use(express.methodOverride());
 app.use(app.router);
 app.use(express.static(__dirname + '/public'));
 
-app.get('/service/change', function(req, res) {
-	console.log("*** Color Changed ***");
-	console.log("Red: " + req.query.red + " Green: " + req.query.green + " Blue: " + req.query.blue);
-	res.send("ok");
+app.get('/service/changeRGB', function(req, res) {
+	var buffer = '' + req.query.red + ',' + req.query.green + ',' + req.query.blue + '\n';
+	console.log(buffer);
+	
+	sp.write(buffer, function(err) {
+		if (err) {
+			console.log('Error sending data to serial port: ' + err);
+		}
+	});
+	
+	res.send('Buffer sent: ' + buffer);
 });
 
 http.createServer(app).listen(app.get('port'), function() {
 	console.log('Express server listening on port ' + app.get('port'));
+	
+	sp.open(function(err) {
+		if (!err) {
+			console.log('Serial port open');
+		} else {
+			console.log('Error open serial port: ' + err);
+		}
+	});
 });
